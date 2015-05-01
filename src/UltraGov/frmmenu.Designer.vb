5ae0abbc806cf2c6e72a8659facca3232d931d95
Freuvim <kelvin_tesche@hotmail.com>
2015-05-01 17:05:28 -0300

    inicio
    teste

---
+++ src/UltraGov/frmmenu.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmenu
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmmenu))
+        Me.btcertificado = New MetroFramework.Controls.MetroButton()
+        Me.btativa = New MetroFramework.Controls.MetroButton()
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.cmsmenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.cmsabrir = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmsativa = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmssair = New System.Windows.Forms.ToolStripMenuItem()
+        Me.pbnotifi = New System.Windows.Forms.PictureBox()
+        Me.pbfirefox = New System.Windows.Forms.PictureBox()
+        Me.pbconfig = New System.Windows.Forms.PictureBox()
+        Me.ttinfo = New MetroFramework.Components.MetroToolTip()
+        Me.pbinfo = New System.Windows.Forms.PictureBox()
+        Me.cmsmenu.SuspendLayout()
+        CType(Me.pbnotifi, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbfirefox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbconfig, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbinfo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btcertificado
+        '
+        Me.btcertificado.DisplayFocus = True
+        Me.btcertificado.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        resources.ApplyResources(Me.btcertificado, "btcertificado")
+        Me.btcertificado.Name = "btcertificado"
+        Me.btcertificado.TabStop = False
+        Me.btcertificado.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.ttinfo.SetToolTip(Me.btcertificado, resources.GetString("btcertificado.ToolTip"))
+        Me.btcertificado.UseCustomBackColor = True
+        Me.btcertificado.UseSelectable = True
+        '
+        'btativa
+        '
+        Me.btativa.DisplayFocus = True
+        Me.btativa.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        resources.ApplyResources(Me.btativa, "btativa")
+        Me.btativa.Name = "btativa"
+        Me.btativa.Style = MetroFramework.MetroColorStyle.Blue
+        Me.btativa.TabStop = False
+        Me.btativa.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.ttinfo.SetToolTip(Me.btativa, resources.GetString("btativa.ToolTip"))
+        Me.btativa.UseCustomBackColor = True
+        Me.btativa.UseSelectable = True
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        resources.ApplyResources(Me.NotifyIcon, "NotifyIcon")
+        Me.NotifyIcon.ContextMenuStrip = Me.cmsmenu
+        '
+        'cmsmenu
+        '
+        Me.cmsmenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.cmsabrir, Me.cmsativa, Me.cmssair})
+        Me.cmsmenu.Name = "cmsmenu"
+        resources.ApplyResources(Me.cmsmenu, "cmsmenu")
+        '
+        'cmsabrir
+        '
+        Me.cmsabrir.Name = "cmsabrir"
+        resources.ApplyResources(Me.cmsabrir, "cmsabrir")
+        '
+        'cmsativa
+        '
+        Me.cmsativa.Name = "cmsativa"
+        resources.ApplyResources(Me.cmsativa, "cmsativa")
+        '
+        'cmssair
+        '
+        Me.cmssair.Name = "cmssair"
+        resources.ApplyResources(Me.cmssair, "cmssair")
+        '
+        'pbnotifi
+        '
+        Me.pbnotifi.Cursor = System.Windows.Forms.Cursors.Hand
+        resources.ApplyResources(Me.pbnotifi, "pbnotifi")
+        Me.pbnotifi.Name = "pbnotifi"
+        Me.pbnotifi.TabStop = False
+        Me.ttinfo.SetToolTip(Me.pbnotifi, resources.GetString("pbnotifi.ToolTip"))
+        '
+        'pbfirefox
+        '
+        Me.pbfirefox.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pbfirefox.Image = Global.UltraGov.My.Resources.Resources.firefox
+        resources.ApplyResources(Me.pbfirefox, "pbfirefox")
+        Me.pbfirefox.Name = "pbfirefox"
+        Me.pbfirefox.TabStop = False
+        Me.ttinfo.SetToolTip(Me.pbfirefox, resources.GetString("pbfirefox.ToolTip"))
+        '
+        'pbconfig
+        '
+        Me.pbconfig.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pbconfig.Image = Global.UltraGov.My.Resources.Resources.controls
+        resources.ApplyResources(Me.pbconfig, "pbconfig")
+        Me.pbconfig.Name = "pbconfig"
+        Me.pbconfig.TabStop = False
+        Me.ttinfo.SetToolTip(Me.pbconfig, resources.GetString("pbconfig.ToolTip"))
+        '
+        'ttinfo
+        '
+        Me.ttinfo.Style = MetroFramework.MetroColorStyle.Blue
+        Me.ttinfo.StyleManager = Nothing
+        Me.ttinfo.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'pbinfo
+        '
+        Me.pbinfo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pbinfo.Image = Global.UltraGov.My.Resources.Resources.info
+        resources.ApplyResources(Me.pbinfo, "pbinfo")
+        Me.pbinfo.Name = "pbinfo"
+        Me.pbinfo.TabStop = False
+        Me.ttinfo.SetToolTip(Me.pbinfo, resources.GetString("pbinfo.ToolTip"))
+        '
+        'frmmenu
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle
+        Me.Controls.Add(Me.pbinfo)
+        Me.Controls.Add(Me.btcertificado)
+        Me.Controls.Add(Me.btativa)
+        Me.Controls.Add(Me.pbconfig)
+        Me.Controls.Add(Me.pbfirefox)
+        Me.Controls.Add(Me.pbnotifi)
+        Me.MaximizeBox = False
+        Me.Name = "frmmenu"
+        Me.Resizable = False
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.cmsmenu.ResumeLayout(False)
+        CType(Me.pbnotifi, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbfirefox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbconfig, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbinfo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btcertificado As MetroFramework.Controls.MetroButton
+    Friend WithEvents btativa As MetroFramework.Controls.MetroButton
+    Friend WithEvents NotifyIcon As System.Windows.Forms.NotifyIcon
+    Friend WithEvents cmsmenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents cmsabrir As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents cmsativa As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents cmssair As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents pbnotifi As System.Windows.Forms.PictureBox
+    Friend WithEvents pbfirefox As System.Windows.Forms.PictureBox
+    Friend WithEvents pbconfig As System.Windows.Forms.PictureBox
+    Friend WithEvents ttinfo As MetroFramework.Components.MetroToolTip
+    Friend WithEvents pbinfo As System.Windows.Forms.PictureBox
+
+End Class
---
+++ src/UltraGov/frmsobre.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmsobre
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmsobre))
+        Me.pblogo = New System.Windows.Forms.PictureBox()
+        Me.btok = New MetroFramework.Controls.MetroButton()
+        Me.tlpsobre = New System.Windows.Forms.TableLayoutPanel()
+        Me.rtbsobre = New System.Windows.Forms.RichTextBox()
+        CType(Me.pblogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.tlpsobre.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pblogo
+        '
+        Me.pblogo.Image = Global.UltraGov.My.Resources.Resources.Logo
+        Me.pblogo.Location = New System.Drawing.Point(279, 23)
+        Me.pblogo.Name = "pblogo"
+        Me.pblogo.Size = New System.Drawing.Size(136, 31)
+        Me.pblogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pblogo.TabIndex = 0
+        Me.pblogo.TabStop = False
+        '
+        'btok
+        '
+        Me.btok.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btok.FontSize = MetroFramework.MetroButtonSize.Medium
+        Me.btok.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        Me.btok.Location = New System.Drawing.Point(317, 300)
+        Me.btok.Name = "btok"
+        Me.btok.Size = New System.Drawing.Size(75, 23)
+        Me.btok.TabIndex = 1
+        Me.btok.TabStop = False
+        Me.btok.Text = "&OK"
+        Me.btok.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.btok.UseSelectable = True
+        '
+        'tlpsobre
+        '
+        Me.tlpsobre.ColumnCount = 1
+        Me.tlpsobre.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tlpsobre.Controls.Add(Me.btok, 0, 1)
+        Me.tlpsobre.Controls.Add(Me.rtbsobre, 0, 0)
+        Me.tlpsobre.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tlpsobre.Location = New System.Drawing.Point(20, 60)
+        Me.tlpsobre.Name = "tlpsobre"
+        Me.tlpsobre.RowCount = 2
+        Me.tlpsobre.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 297.0!))
+        Me.tlpsobre.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23.0!))
+        Me.tlpsobre.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.tlpsobre.Size = New System.Drawing.Size(395, 326)
+        Me.tlpsobre.TabIndex = 2
+        '
+        'rtbsobre
+        '
+        Me.rtbsobre.BackColor = System.Drawing.Color.FromArgb(CType(CType(34, Byte), Integer), CType(CType(34, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.rtbsobre.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtbsobre.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.rtbsobre.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbsobre.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtbsobre.ForeColor = System.Drawing.Color.White
+        Me.rtbsobre.Location = New System.Drawing.Point(3, 3)
+        Me.rtbsobre.Name = "rtbsobre"
+        Me.rtbsobre.ReadOnly = True
+        Me.rtbsobre.Size = New System.Drawing.Size(389, 291)
+        Me.rtbsobre.TabIndex = 2
+        Me.rtbsobre.TabStop = False
+        Me.rtbsobre.Text = resources.GetString("rtbsobre.Text")
+        '
+        'frmsobre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle
+        Me.ClientSize = New System.Drawing.Size(435, 406)
+        Me.Controls.Add(Me.tlpsobre)
+        Me.Controls.Add(Me.pblogo)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmsobre"
+        Me.Resizable = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Sobre"
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
+        CType(Me.pblogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.tlpsobre.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pblogo As System.Windows.Forms.PictureBox
+    Friend WithEvents btok As MetroFramework.Controls.MetroButton
+    Friend WithEvents tlpsobre As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents rtbsobre As System.Windows.Forms.RichTextBox
+End Class
